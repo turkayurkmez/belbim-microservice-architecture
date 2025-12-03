983a112262674f72627e86db92c391d8b6b56832
türkay ürkmez <turkay.urkmez@outlook.com>
2025-12-03 14:18:02 +0300

    Add domain event mechanism to AggregateRoot
    Introduced functionality in `AggregateRoot<T>` to manage domain
    events, including methods to add, clear, and access events.
    
    Added a new `DomainEvent` base class to define domain events,
    implementing `IDomainEvent` with properties for unique ID and
    occurrence timestamp.
    
    These changes enable better tracking and handling of domain
    events within the project.

--- BelbimEShop/src/Shared/BelbimEShop.Shared.Library/Domain/AggregateRoot.cs
+++ BelbimEShop/src/Shared/BelbimEShop.Shared.Library/Domain/AggregateRoot.cs
@@ -6,8 +6,24 @@ using System.Threading.Tasks;
 
 namespace BelbimEShop.Shared.Library.Domain
 {
-    public abstract class AggregateRoot<T> :Entity<T>, IAggregateRoot where T: struct, IEquatable<T>
+
+    /*
+     * Tüm domain olaylarını yöneten ve "aggregate" olarak tanımlanabilen her entity, bazı kuralları barındırmak zorunda.
+     * O kuralları da bu abstract class'da tanımlıyoruz.
+     */
+    public abstract class AggregateRoot<T> : Entity<T>, IAggregateRoot where T : struct, IEquatable<T>
     {
+        private readonly List<IDomainEvent> domainEvents = new List<IDomainEvent>();
+        public IReadOnlyCollection<IDomainEvent> DomainEvents => domainEvents;
+
+        public void ClearDomainEvents()
+        {
+            domainEvents.Clear();
+        }
+
+        public void AddDomainEvent(IDomainEvent domainEvent) { 
+            domainEvents.Add(domainEvent);
+        }
 
     }
 }
